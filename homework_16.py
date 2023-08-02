89e0b5b4944bac7cee7be252bca0fb6b30ade7db
blt1305 <blt1305@gmail.com>
2023-08-02 22:01:11 +0300

    Создан базовый шаблон. Произведено наследование от базового в index.html и about.html.

--- homework_16.py
+++ homework_16.py
@@ -1,19 +1,16 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+menu = ['Установка', 'Первое приложение', 'Обратная связь']
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',  menu = menu)
 
 
-# @app.route('/')
-# def hello_world():
-#     return '<p>Hello, World!</p>'
-
-@app.route('/again')
-def again():
-    return '<h1>I am glad to see you.</h1>'
+@app.route('/about')
+def about():
+    return render_template('about.html', title = 'О сайте', menu = menu)
 
 if __name__ == '__main__':
     app.run(debug=True)
